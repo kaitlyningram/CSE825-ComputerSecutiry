e289fc0acd3c8514c7344547dcd24010222540ba
kaitlyningram <91685557+kaitlyningram@users.noreply.github.com>
2021-10-04 23:33:15 -0400

    Update DES.py

--- DES.py
+++ DES.py
@@ -42,6 +42,11 @@ e_bit_table   = [ 32,  1,  2,  3,  4,  5,
 				  20, 21, 22, 23, 24, 25,
 				  24, 25, 26, 27, 28, 29,
 				  28, 29, 30, 31, 32,  1 ]
+				  
+s1 = [[14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7],
+	  [ 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8],
+	  [ 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0],
+	  [15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13]]
 
 shift_num = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 	
@@ -69,9 +74,9 @@ def generate_subkeys():
 	
 	permuted_keys = []
 	
-	key48 = '0b'
 	for n in range(16):
 		combined_str = left_blocks[n][2:] + right_blocks[n][2:]
+		key48 = '0b'
 		for c in permute_key48:
 			key48 = key48 + combined_str[int(c)-1]
 		permuted_keys += [key48]
@@ -90,7 +95,7 @@ def encode(plaintext):
 	
 	return ciphertext
 	
-def decode(ciphertext):
+def decode(ciphertext, permuted_keys):
 	ciphertext = format(ciphertext, '#066b')
 	plaintext = ''
 	ip = '0b'
@@ -100,9 +105,57 @@ def decode(ciphertext):
 
 	left_key  = ip[:34]
 	right_key = '0b' + ip[34:70]
+	
+	left_blocks  = []
+	right_blocks = []
+	
+	for n in range(16):
+		left_blocks  = [right_key]
+		f = '0b'
+		for c in e_bit_table:
+			f = f + right_key[int(c)+1]
+			
+		right = xor(f, permuted_keys[n]) # this is K_n xor E(R_n-1)
+		# use s-blocks
+		s_output = s_block(s1, str(right[2:8]))
+		s_output = s_output + s_block(s1, right[8:14])[2:]
+		s_output = s_output + s_block(s1, right[14:20])[2:]
+		s_output = s_output + s_block(s1, right[20:26])[2:]
+		s_output = s_output + s_block(s1, right[26:32])[2:]
+		s_output = s_output + s_block(s1, right[32:38])[2:]
+		s_output = s_output + s_block(s1, right[38:44])[2:]
+		s_output = s_output + s_block(s1, right[44:50])[2:]
+		
+		print(s_output)
 
 	return plaintext
-
+	
+def xor(string1, string2):
+	str = '0b'
+	
+	# remove '0b' from start of binary strings
+	string1 = string1[2:]
+	string2 = string2[2:]
+	
+	for n in range(len(string1)):
+		if string1[n] != string2[n]:
+			str = str + '1'
+		else:
+			str = str + '0'
+			
+	return str
+	
+def s_block(s_n, bits):
+	print(len(bits))
+	row = bits[0] + bits[5]
+	col = bits[1:5]
+	
+	print(row)
+	row = int(row, 2)
+	col = int(col, 2)
+	
+	return format(s_n[row][col], '#06b')
+	
 # parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--encrypt', action='store_true', help='encrypt the text')
@@ -134,5 +187,5 @@ if args.encrypt:
 	
 if args.decrypt:
 	print('Decrypting text: ' + args.text)
-	decode(text)
+	decode(text, permute_keys)
 
