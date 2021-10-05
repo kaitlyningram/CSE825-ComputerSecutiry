5b71605a8d350cb6f5cbfe1ec0c4a8fc12de3e4c
kaitlyningram <91685557+kaitlyningram@users.noreply.github.com>
2021-10-04 21:06:42 -0400

    Update DES.py

--- DES.py
+++ DES.py
@@ -8,21 +8,60 @@
 import argparse
 
 key = 0x133457799bbcdff1
-permute_key = [ 57, 49, 41, 33, 25, 17,  9,
-				 1, 58, 50, 42, 34, 26, 18,
-				10,  2, 59, 51, 43, 35, 27,
-				19, 11,  3, 60, 52, 44, 36,
-				63, 55, 47, 39, 31, 23, 15,
-				 7, 62, 54, 46, 38, 30, 22,
-				14,  6, 61, 53, 45, 37, 29,
-				21, 13,  5, 28, 20, 12,  4]
-								
+permute_key56 = [ 57, 49, 41, 33, 25, 17,  9,
+				   1, 58, 50, 42, 34, 26, 18,
+				  10,  2, 59, 51, 43, 35, 27,
+				  19, 11,  3, 60, 52, 44, 36,
+				  63, 55, 47, 39, 31, 23, 15,
+				   7, 62, 54, 46, 38, 30, 22,
+				  14,  6, 61, 53, 45, 37, 29,
+				  21, 13,  5, 28, 20, 12,  4 ]
+				
+permute_key48 = [ 14, 17, 11, 24,  1,  5,
+				   3, 28, 15,  6, 21, 10,
+				  23, 19, 12,  4, 26,  8,
+				  16,  7, 27, 20, 13,  2,
+				  41, 52, 31, 37, 47, 55,
+				  30, 40, 51, 45, 33, 48,
+				  44, 49, 39, 56, 34, 53,
+				  46, 42, 50, 36, 29, 32 ]
+				
+shift_num = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+	
+left_blocks  = []
+right_blocks = []
+	
 def permute_56bit():
 	str = '0b'
-	for c in permute_key:
+	for c in permute_key56:
 		# generate 56 bit key
 		str = str + key[int(c)+1]
 	return str
+	
+def permute_48bit():
+	str = '0b'
+	permuted_keys = []
+	for n in range(16):
+		combined_str = left_blocks[n+1][2:] + right_blocks[n+1][2:]
+		for c in permute_key48:
+			# generate 48 bit key
+			str = str + combined_str[int(c)-1]
+		permuted_keys += [str]
+	return permuted_keys
+	
+def generate_28bit(left_key, right_key):
+	left_blocks  = []
+	right_blocks = []
+	
+	left_blocks  += [left_key]
+	right_blocks += [right_key]
+	
+	for n in range(16):		
+		left_key, right_key = shift_key(left_key, right_key, shift_num[n])
+		left_blocks  += [left_key]
+		right_blocks += [right_key]
+	
+	return left_blocks, right_blocks
 
 def shift_key(left_key, right_key, shift):
 	left_key  = '0b' + left_key[2+shift:30] + left_key[2:2+shift]
@@ -68,9 +107,9 @@ if args.encrypt:
 	print('C   ' + str(left_key))
 	print('D   ' + str(right_key))
 	
-	left_key, right_key = shift_key(left_key, right_key, 1)
-	left_key, right_key = shift_key(left_key, right_key, 1)
-	left_key, right_key = shift_key(left_key, right_key, 2)
+	left_blocks, right_blocks = generate_28bit(left_key, right_key)
+	
+	permute_keys = permute_48bit()
 	
 if args.decrypt:
 	print('Decrypting text: ' + args.text)
