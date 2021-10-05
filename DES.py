1c7108ec6394e33387101e5de50ecc2536c44940
kaitlyningram <91685557+kaitlyningram@users.noreply.github.com>
2021-10-04 22:21:36 -0400

    Update DES.py

--- DES.py
+++ DES.py
@@ -34,6 +34,15 @@ init_permute  = [ 58, 50, 42, 34, 26, 18, 10,  2,
 				  61, 53, 45, 37, 29, 21, 13,  5,
 				  63, 55, 47, 39, 31, 23, 15,  7 ]
 
+e_bit_table   = [ 32,  1,  2,  3,  4,  5,
+				   4,  5,  6,  7,  8,  9,
+				   8,  9, 10, 11, 12, 13,
+				  12, 13, 14, 15, 16, 17,
+				  16, 17, 18, 19, 20, 21,
+				  20, 21, 22, 23, 24, 25,
+				  24, 25, 26, 27, 28, 29,
+				  28, 29, 30, 31, 32,  1 ]
+
 shift_num = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 	
 def generate_subkeys():
@@ -85,7 +94,6 @@ def decode(ciphertext):
 	ciphertext = format(ciphertext, '#066b')
 	plaintext = ''
 	ip = '0b'
-	print(ciphertext)
 	
 	for c in init_permute:
 		ip = ip + ciphertext[int(c)+1]
